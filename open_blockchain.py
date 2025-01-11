6bd2719cc237ad27e8dee4ffdbec4670c427a977
virginviolet <148799361+virginviolet@users.noreply.github.com>
2025-01-11 04:09:08 +0100

    flask database test

--- open_blockchain.py
+++ open_blockchain.py
@@ -1,9 +1,24 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, request, jsonify
+from typing import List
 
-@app.route('/')
-def home():
-    return "Hello, world!"
+app = Flask(__name__)
+database: List[str] = []
+
+
+@app.route('/add_entry', methods=['POST'])
+def add_entry():
+    data = request.get_json().get("data")
+    if not data:
+        return jsonify({"error": "No data provided."}), 400
+
+    database.append(data)  # Add the data to our simple list
+    return jsonify({"message": "Data added!", "data": data}), 200
+
+
+@app.route('/get_database', methods=['Get'])
+def get_database():
+    return jsonify({"entries": database, "count": len(database)}), 200
+
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
