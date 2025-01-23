d10973276a1e01e9714fc7f7c364840caa79a544
virginviolet <148799361+virginviolet@users.noreply.github.com>
2025-01-23 19:46:02 +0100

    Working on adding slot machine

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+
 import sb_blockchain
 import threading
 import subprocess
@@ -6,6 +7,7 @@ import asyncio
 import json
 import pytz
 import random
+import math
 from time import sleep, time
 from datetime import datetime
 from discord import Guild, Intents, Interaction, Member, Message, Client, Emoji, PartialEmoji, User, TextChannel, app_commands
@@ -16,7 +18,8 @@ from os.path import exists
 from dotenv import load_dotenv
 from hashlib import sha256
 from sys import exit as sys_exit
-from typing import Dict, List, NoReturn, TextIO, cast
+from collections import namedtuple
+from typing import Dict, List, NoReturn, TextIO, cast, NamedTuple
 
 
 # region Variables
@@ -33,6 +36,7 @@ channel_checkpoint_limit: int = 3
 # endregion
 
 # region Checkpoints
+
 
 class ChannelCheckpoints:
     def __init__(self,
@@ -56,11 +60,11 @@ class ChannelCheckpoints:
     def count_lines(self) -> int:
         if not exists(self.file_name):
             return 0
-        
+
         with open(self.file_name, "r") as file:
             count: int = sum(1 for _ in file)
             return count
-        
+
     def create(self) -> None:
         # Create missing directories
         directories: str = self.file_name[:self.file_name.rfind("/")]
@@ -115,7 +119,7 @@ class ChannelCheckpoints:
             return None
 
         with open(self.file_name, "r") as file:
-            checkpoints: List[Dict[str,int]] | None = []
+            checkpoints: List[Dict[str, int]] | None = []
             # print("Loading checkpoints...")
             for line in file:
                 checkpoint: Dict[str, int] = (
@@ -132,7 +136,7 @@ class ChannelCheckpoints:
 class Log:
     '''
     The log cannot currently be verified or generated from the blockchain.
-    Use a validated transactions file for verification (see 
+    Use a validated transactions file for verification (see
     Blockchain.validate_transactions_file()).
     The log is meant to be a local record of events.
     '''
@@ -151,7 +155,7 @@ class Log:
     def create(self) -> None:
         # Create missing directories
         directories: str = self.file_name[:self.file_name.rfind("/")]
-        for directory in directories.split("/"):
+        for i, directory in enumerate(directories.split("/")):
             if not exists(directory):
                 makedirs(directory)
 
@@ -198,11 +202,14 @@ class BotConfiguration:
         self.coins: str = self.configuration["COINS"]
         self.coin_emoji_id: int = int(self.configuration["COIN_EMOJI_ID"])
         if self.coin_emoji_id == 0:
-            print("WARNING: COIN_EMOJI_ID has not set in bot_configuration.json nor "
+            print("WARNING: COIN_EMOJI_ID has not set "
+                  "in bot_configuration.json nor "
                   "in the environment variables.")
-        self.administrator_id: int = int(self.configuration["ADMINISTRATOR_ID"])
+        self.administrator_id: int = int(
+            self.configuration["ADMINISTRATOR_ID"])
         if self.administrator_id == 0:
-            print("WARNING: ADMINISTRATOR_ID has not set in bot_configuration.json nor "
+            print("WARNING: ADMINISTRATOR_ID has not set "
+                  "in bot_configuration.json nor "
                   "in the environment variables.")
 
     def create(self) -> None:
@@ -218,30 +225,96 @@ class BotConfiguration:
             "COIN": "coin",
             "COINS": "coins",
             "COIN_EMOJI_ID": "0",
-            "ADMINISTRATOR_ID": "0"
+            "AWARD_LOSE_EMOJI": "0",
+            "AWARD_SMALL_WIN_EMOJI": "0",
+            "AWARD_MEDIUM_WIN_EMOJI": "0",
+            "AWARD_HIGH_WIN_EMOJI": "0",
+            "AWARD_VERY_HIGH_WIN_EMOJI": "0",
+            "AWARD_JACKPOT_EMOJI": "0",
+            "CASINO_HOUSE_ID": "0",
+            "ADMINISTRATOR_ID": "0",
         }
         # Save the configuration to the file
         with open(self.file_name, "w") as f:
             f.write(json.dumps(configuration))
-    
+
     def read(self) -> Dict[str, str]:
         if not exists(self.file_name):
             self.create()
-        
+
         with open(self.file_name, "r") as f:
             configuration: Dict[str, str] = json.loads(f.read())
             # Override the configuration with environment variables
-            if os_environ.get("COIN"):
-                configuration["coin"] = os_environ.get("COIN", "")
-            if os_environ.get("COINS"):
-                configuration["coins"] = os_environ.get("COINS", "")
-            if os_environ.get("COIN_EMOJI_ID"):
-                configuration["COIN_EMOJI_ID"] = os_environ.get("COIN_EMOJI_ID", "")
-            if os_environ.get("ADMINISTRATOR_ID"):
-                configuration["ADMINISTRATOR_ID"] = os_environ.get("ADMINISTRATOR_ID", "")
+            env_vars: Dict[str, str] = {
+                "COIN": "coin",
+                "COINS": "coins",
+                "COIN_EMOJI_ID": "COIN_EMOJI_ID",
+                "AWARD_LOSE_EMOJI": "AWARD_LOSE_EMOJI",
+                "AWARD_SMALL_WIN_EMOJI": "AWARD_SMALL_WIN_EMOJI",
+                "AWARD_MEDIUM_WIN_EMOJI": "AWARD_MEDIUM_WIN_EMOJI",
+                "AWARD_HIGH_WIN_EMOJI": "AWARD_HIGH_WIN_EMOJI",
+                "AWARD_VERY_HIGH_WIN_EMOJI": "AWARD_VERY_HIGH_WIN_EMOJI",
+                "AWARD_JACKPOT_EMOJI": "AWARD_JACKPOT_EMOJI",
+                "CASINO_HOUSE_ID": "CASINO_HOUSE_ID",
+                "ADMINISTRATOR_ID": "ADMINISTRATOR_ID"
+            }
+
+            for env_var, config_key in env_vars.items():
+                if os_environ.get(env_var):
+                    configuration[config_key] = os_environ.get(env_var, "")
+
             return configuration
-            
-            
+
+
+# endregion
+
+# region SaveData
+class SaveData:
+    def __init__(self, user_id: int, user_name: str) -> None:
+        self.user_id: int = user_id
+        self.user_name: str = user_name
+        self.file_name: str = f"data/save_data/{user_id}.json"
+        self.starting_bonus_received: bool = False
+
+    def create(self) -> None:
+        # Create missing directories
+        directories: str = self.file_name[:self.file_name.rfind("/")]
+        for i, directory in enumerate(directories.split("/")):
+            path: str = "/".join(directories.split("/")[:i+1])
+            if not exists(directory):
+                makedirs(path, exist_ok=True)
+            if directory.isdigit() and int(directory) == self.user_id:
+                name_file_name: str = f"{path}/user_name.json"
+                with open(name_file_name, "w") as file:
+                    file.write(json.dumps(
+                        {"user_name": self.user_name,
+                        "user_id": self.user_id,
+                        "starting_bonus_received": (
+                            self.starting_bonus_received)
+                        }))
+                    pass
+
+        # Create the save data file
+        with open(self.file_name, "w"):
+            pass
+
+    def save(self, key: str, value: str) -> None:
+        if not exists(self.file_name):
+            self.create()
+
+        with open(self.file_name, "w") as f:
+            f.write(json.dumps({key: value}))
+
+    def load(self, key: str) -> str | None:
+        if not exists(self.file_name):
+            return None
+
+        all_data: Dict[str, str] = {}
+        with open(self.file_name, "r") as file:
+            for line in file:
+                data: Dict[str, str] = json.loads(line)
+                all_data.update(data)
+            return all_data[key]
 # endregion
 
 # region Flask funcs
@@ -297,6 +370,7 @@ def start_flask_app() -> None:
 
 # region CP start
 
+
 def start_checkpoints(limit: int = 10) -> Dict[int, ChannelCheckpoints]:
     all_checkpoints: dict[int, ChannelCheckpoints] = {}
     print("Starting checkpoints...")
@@ -338,11 +412,13 @@ async def process_missed_messages() -> None:
     print("Processing missed messages...")
 
     for guild in bot.guilds:
-        print(f"Fetching messages from guild: {guild.name} ({guild.id})...")
+        print("Fetching messages from "
+              f"guild: {guild.name} ({guild.id})...")
         for channel in guild.text_channels:
             print("Fetching messages from "
                   f"channel: {channel.name} ({channel.id})...")
-            channel_checkpoints: List[Dict[str, int]] | None = all_channel_checkpoints[channel.id].load()
+            channel_checkpoints: List[Dict[str, int]] | None = (
+                all_channel_checkpoints[channel.id].load())
             if channel_checkpoints is not None:
                 print(f"Channel checkpoints loaded.")
             else:
@@ -376,18 +452,20 @@ async def process_missed_messages() -> None:
                             receiver: User | Member = message.author
                             emoji: PartialEmoji | Emoji | str = reaction.emoji
                             await process_reaction(emoji, sender, receiver)
-            print(f"Messages from channel {channel.name} ({channel.id}) fetched.")
+            print("Messages from "
+                  f"channel {channel.name} ({channel.id}) fetched.")
             if fresh_last_message_id is None:
                 print("WARNING: No channel messages found.")
             else:
                 if new_channel_messages_found > 0:
-                    # TODO Only save checkpoint if we read any messages beyond the
-                    # already saved checkpoint
+                    # TODO Only save checkpoint if we read any messages beyond
+                    # the already saved checkpoint
                     print(f"Saving checkpoint: {fresh_last_message_id}")
-                    all_channel_checkpoints[channel.id].save(fresh_last_message_id)
+                    all_channel_checkpoints[channel.id].save(
+                        fresh_last_message_id)
                 else:
-                    print("Will not save checkpoint for this channel because no "
-                        "new messages were found.")
+                    print("Will not save checkpoint for this channel because "
+                          "no new messages were found.")
         print(f"Messages from guild {guild.id} ({guild}) fetched.")
     print(missed_messages_processed_message)
 
@@ -412,7 +490,7 @@ async def process_reaction(emoji: PartialEmoji | Emoji | str,
         case str():
             return
     if emoji_id == COIN_EMOJI_ID:
-        
+
         if receiver is None:
             # Get receiver from id
             if receiver_id is not None:
@@ -422,19 +500,18 @@ async def process_reaction(emoji: PartialEmoji | Emoji | str,
                 return
         else:
             receiver_id = receiver.id
-        
+
         sender_id: int = sender.id
 
         print(f"{sender} ({sender_id}) is mining 1 {COIN} "
               f"for {receiver} ({receiver_id})...")
         await add_block_transaction(
             blockchain=blockchain,
-            sender = sender,
-            receiver = receiver,
+            sender=sender,
+            receiver=receiver,
             amount=1,
             method="reaction"
         )
-    
 
         # Log the mining
         block_retrieval_success: bool | None = None
@@ -558,11 +635,18 @@ COIN: str = configuration.coin
 COINS: str = configuration.coins
 COIN_EMOJI_ID: int = configuration.coin_emoji_id
 ADMINISTRATOR_ID: int = configuration.administrator_id
+AWARD_LOSE_EMOJI: int = configuration.award_lose_emoji
+AWARD_SMALL_WIN_EMOJI: int = configuration.award_small_win_emoji
+AWARD_MEDIUM_WIN_EMOJI: int = configuration.award_medium_win_emoji
+AWARD_HIGH_WIN_EMOJI: int = configuration.award_high_win_emoji
+AWARD_VERY_HIGH_WIN_EMOJI: int = configuration.award_very_high_win_emoji
+AWARD_JACKPOT_EMOJI: int = configuration.award_jackpot_emoji
 print("Bot configuration loaded.")
 
 print("Initializing log...")
 log = Log(time_zone="Canada/Central")
 print("Log initialized.")
+
 
 @bot.event
 async def on_ready() -> None:
@@ -571,7 +655,7 @@ async def on_ready() -> None:
     global all_channel_checkpoints
     all_channel_checkpoints = start_checkpoints(limit=channel_checkpoint_limit)
 
-    await process_missed_messages()
+    # await process_missed_messages()
 
     # Sync the commands to Discord
     print("Syncing commands...")
@@ -589,7 +673,7 @@ async def on_ready() -> None:
 async def on_message(message: Message) -> None:
     global all_channel_checkpoints
     channel_id: int = message.channel.id
-    
+
     if channel_id in all_channel_checkpoints:
         all_channel_checkpoints[channel_id].save(message.id)
     else:
@@ -623,7 +707,7 @@ async def on_message(message: Message) -> None:
             await message.channel.send("An error occurred. "
                                        f"{administrator} pls fix.")
             return
-        
+
 # endregion
 
 
@@ -747,6 +831,141 @@ async def balance(interaction: Interaction, user: Member | None = None) -> None:
     else:
         await interaction.response.send_message(f"{user_to_check} has "
                                                 f"{balance} {COINS}.")
+
+# region Pull
+
+
+@bot.tree.command(name="pull",
+                  description="Pull the lever and test your luck")
+@app_commands.describe(wager="Amount of coins to wager")
+async def pull(interaction: Interaction, wager: int | None = None) -> None:
+    """
+    Pull the lever and test your luck. You can win 1-10 coins.
+
+    Args:
+        interaction (Interaction): The interaction object representing the
+        command invocation.
+    """
+    def clamp(value: float, lower_bound: float, upper_bound: float) -> float:
+        '''
+        Clamp a value between a minimum and a maximum value.
+        '''
+        return max(lower_bound, min(value, upper_bound))
+    
+    if wager is None:
+        wager = 1
+    user: User | Member = interaction.user
+    user_id: int = user.id
+    user_name: str = user.name
+    save_data: SaveData = SaveData(user_id=user_id, user_name=user_name)
+    starting_bonus_received: bool = save_data.load("starting_bonus_received") == "True"
+    class Award(NamedTuple):
+        emoji: int | str
+        min: int
+        max: int
+        chance: float
+
+    return_to_player: float = 0.95
+    # Decrase the chance of losing the more you wager
+    # Increase the chance of winning the more you wager
+    # Add a small value to avoid log(0), which is undefined and would cause a math error.
+    wager_logarithm: float = math.log(wager + 0.001)
+    # chance is affected by wager and RTP
+    lose = Award(emoji=AWARD_LOSE_EMOJI,
+                 min=-1,
+                 max=-wager,
+                 chance=(0.5 / (wager_logarithm * 0.1)) * (return_to_player))
+    # frequent small wins
+    small_win = Award(emoji=AWARD_SMALL_WIN_EMOJI,
+                      min=1,
+                      max=1,
+                      chance=clamp(
+                          value=0.3 * (1 - wager_logarithm * 0.015) * return_to_player,
+                          lower_bound=0.3 * return_to_player,
+                          upper_bound=0.7 * return_to_player
+                          ))
+    medium_win = Award(emoji=AWARD_MEDIUM_WIN_EMOJI,
+                       min=2,
+                       max=5,
+                       # if wager is really high, this breaks and become negative for some reason
+                       chance=min(0.3, 0.15 * (800 - wager_logarithm * 0.01)) * return_to_player)
+    # min and max values are affected by wager
+    high_win = Award(emoji=AWARD_HIGH_WIN_EMOJI,
+                     min=,
+                     max=,
+                     chance=
+    very_high_win = Award(emoji=AWARD_VERY_HIGH_WIN_EMOJI,
+                          min=85,
+                          max=200, 
+                          chance=
+    """ jackpot = Award(emoji=AWARD_JACKPOT_EMOJI,
+                    min=,
+                    max=,
+                    chance=) """
+    
+    slot_results: List[Award]= []
+    slot_results_emojis: List[PartialEmoji | Emoji | str] = []
+    reward_amount: int = 0
+    message_one_sent: bool = False
+    if not starting_bonus_received:
+        # Send message to inform user of starting bonus
+        await interaction.response.send_message(
+            f"The first time you play, you are guaranteed to win!")
+        message_one_sent = True
+        slot_results = [very_high_win, very_high_win, very_high_win]
+    else:
+        for _ in range(3):
+            slot_results.append(random.choice([lose, small_win, medium_win, high_win, very_high_win]))
+    # convert emoji id to emoji
+    print(f"Slot results: {slot_results}")
+    for slot in slot_results:
+        if isinstance(slot.emoji, int):
+            emoji: Emoji | None = bot.get_emoji(slot.emoji)
+            if emoji is not None:
+                slot_results_emojis.append(emoji)
+            else:
+                # TODO Refactor into function
+                print(f"Error: Emoji not found for {slot.emoji}")
+                administrator: User = (
+                    await bot.fetch_user(ADMINISTRATOR_ID))
+                await interaction.response.send_message(
+                    f"Error: Emoji not found for {slot.emoji}. "
+                    f"{administrator.mention} pls fix.")
+                return
+
+        else:
+            slot_results_emojis.append(slot.emoji)
+    print(slot_results_emojis)
+    if slot_results[0] == slot_results[1] == slot_results[2]:
+        reward_amount = random.randint(slot_results[0].min, slot_results[0].max)
+    message = f"{user.mention} Sorry, you lost {wager} {COINS}."
+    timestamp: float = time()
+    if not starting_bonus_received:
+        await interaction.followup.send("You won a starting bonus!")
+        message = f"You won {reward_amount} {COINS}!"
+        log.log(
+            line=f"{user_name} ({user_id}) pulled the lever "
+                f"and received a starting bonus of {reward_amount} {COINS}.",
+            timestamp=timestamp)
+    elif reward_amount < 1:
+        log.log(
+            line=f"{user_name} ({user_id}) pulled the lever "
+                 f"and lost {wager} {COINS}.",
+            timestamp=timestamp)
+    # TODO Add proper timestamp
+    # TODO Send gif of slot machine
+    else:
+        message = f"Congratulations! You won {reward_amount} {COINS}!"
+    else:
+        reward_amount_positive: int = abs(reward_amount)
+        message: str = f"Sorry, you lost {reward_amount_positive} {COINS}."
+    await interaction.response.send_message(message)
+
+    if not starting_bonus_received:
+        save_data.save("starting_bonus_received", "True")
+
+# endregion
+
 
 # region Message
 # Example slash command
