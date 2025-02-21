2f0c21d59ee9a47ecf2c85fa56e605adccc0a58f
virginviolet <148799361+virginviolet@users.noreply.github.com>
2025-02-21 22:40:14 +0100

    Added PowerShell script for uploading bot config

---
+++ scripts/upload_bot_config.ps1
@@ -0,0 +1,21 @@
+try {
+    Write-Warning "This script will replace the bot config on the server."
+    Pause
+    Import-Module Set-PsEnv
+    
+    Set-PsEnv
+    
+    $scriptDirPath = Split-Path -Parent $MyInvocation.MyCommand.Path
+    $botDirPath = Split-Path -Parent $scriptDirPath
+    $slotMachineConfigPath = "$botDirPath\data\bot_configuration.json"
+    
+    Write-Host "Server URL: $Env:SERVER_URL"
+    Invoke-RestMethod -Uri "$Env:SERVER_URL/set_bot_config" `
+        -Method 'Post' `
+        -Headers @{'token' = $Env:SERVER_TOKEN; "Content-Type"="application/json"} `
+        -Body (Get-Content -Raw -Path $slotMachineConfigPath)
+}
+catch {
+    Write-Host $_
+    Pause
+}
