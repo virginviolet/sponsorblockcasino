a3f50227dc3462ea00cfa499274c3e2d1d42a678
virginviolet <148799361+virginviolet@users.noreply.github.com>
2025-03-05 17:01:26 +0100

    Fixed add block transaction script

--- scripts/add_block (single transaction).ps1
+++ scripts/add_block (single transaction).ps1
@@ -11,7 +11,7 @@ try {
     $transaction = @{
         sender = $from
         receiver = $to
-        amount = 1
+        amount = $amount
         method = "manual"
     }
     Write-Host "Server URL: $Env:SERVER_URL"
